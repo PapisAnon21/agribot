d6190ad4beb64e65af892f654adb890180f372e2
papis <anon@gmail.com>
2024-04-05 18:35:13 +0000

    animation du reservoir succes

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const  terre_cote = document.querySelector(".a-cote-terre")
-
+const reservoir_elt = document.querySelectorAll(".reservoir")
 function display_terre_a_cote()
 {
     terre_cote.classList.add("a-cote-terre-animation")
+    
+}
+
+function display_reservoir()
+{
+    reservoir_elt.forEach(
+        (elt) => {
+            elt.classList.add("animation-reservoir-active")
+        }
+    )
     
 }
