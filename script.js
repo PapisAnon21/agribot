4e1a2df3314c4739ba3e77213835e201ec80162a
papis <anon@gmail.com>
2024-04-07 02:39:28 +0000

    ouf animation des fleche enfin

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const main_tuyau_2 = document.querySelector(".main-tuyau-2")
 const tuyau_secondaire = document.querySelectorAll(".tuyau-secondaire")
 
 
+
+
 function display_terre_a_cote()
 {
     terre_cote.classList.add("a-cote-terre-animation")
@@ -41,4 +43,45 @@ function display_tuyau_secondaire()
             elt.classList.add("tuyau-secondaire-animation-active")
         }
     )
+}
+
+
+
+function display_all_ouverture()
+{
+   // premier lot 1er cote
+    document.querySelector(".fleche-1-1").classList.add("animation-fleche-1-x_1_3-active")
+    document.querySelector(".fleche-1-2").classList.add("animation-fleche-1-x_1_3-active")
+    document.querySelector(".fleche-1-3").classList.add("animation-fleche-1-x_1_3-active")
+
+    // 2e cote
+    document.querySelector(".fleche-1-4").classList.add("animation-fleche-1-x_4_6-active")
+    document.querySelector(".fleche-1-5").classList.add("animation-fleche-1-x_4_6-active")
+    document.querySelector(".fleche-1-6").classList.add("animation-fleche-1-x_4_6-active")
+
+
+
+     // deuxieme lot 1er cote
+     document.querySelector(".fleche-2-1").classList.add("animation-fleche-2-x_1_3-active")
+     document.querySelector(".fleche-2-2").classList.add("animation-fleche-2-x_1_3-active")
+     document.querySelector(".fleche-2-3").classList.add("animation-fleche-2-x_1_3-active")
+ 
+     // 2e cote
+     document.querySelector(".fleche-2-4").classList.add("animation-fleche-2-x_4_6-active")
+     document.querySelector(".fleche-2-5").classList.add("animation-fleche-2-x_4_6-active")
+     document.querySelector(".fleche-2-6").classList.add("animation-fleche-2-x_4_6-active")
+
+
+     // 3 lot 1er cote
+     document.querySelector(".fleche-3-1").classList.add("animation-fleche-3-x_1_3-active")
+     document.querySelector(".fleche-3-2").classList.add("animation-fleche-3-x_1_3-active")
+     document.querySelector(".fleche-3-3").classList.add("animation-fleche-3-x_1_3-active")
+ 
+     // 2e cote
+     document.querySelector(".fleche-3-4").classList.add("animation-fleche-3-x_4_6-active")
+     document.querySelector(".fleche-3-5").classList.add("animation-fleche-3-x_4_6-active")
+     document.querySelector(".fleche-3-6").classList.add("animation-fleche-3-x_4_6-active")
+
+
+    
 }
